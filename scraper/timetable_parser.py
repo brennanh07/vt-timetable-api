2f9f5915fa5333fc90b0fdd4ea226ce8e60d3b74
brennanh07 <brennanh07@vt.edu>
2025-04-27 17:25:04 -0400

    subjects successfully extracted

--- scraper/timetable_parser.py
+++ scraper/timetable_parser.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import re
 from timetable_fetcher import TimetableFetcher
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 class TimetableParser:
-    """Uses bs4 to parse the HTML content fetched from the Virginia Tech Timetable website
-    """
+    """Uses bs4 to parse the HTML content fetched from the Virginia Tech Timetable website"""
+
     def __init__(self, html: str):
         self.soup = BeautifulSoup(html, "html.parser")
         self.subjects = []
@@ -18,20 +22,33 @@ class TimetableParser:
 
         target_script_text = None
         for script_tag in script_tags:
-            if f"case \"{term}\"" in script_tag.text:
+            if f'case "{term}"' in script_tag.text:
                 target_script_text = script_tag.text
 
-        target_term_subjects = re.search(rf'case "{term}" :(.*?)break', target_script_text, re.DOTALL)
-        
+        match = re.search(rf'case "{term}" :(.*?)break', target_script_text, re.DOTALL)
+        if not match:
+            logging.error(f"Error: Could not find script block for term {term}")
+            return
 
-        with open("subjects.html", "w", encoding="utf-8") as f:
-            f.write(target_term_subjects.group(1).strip())
+        # get the first match, and strip empty lines
+        target_term_subjects = match.group(1).strip()
 
+        # remove the first "document.ttform.subj_code.options[0]=..." (all subjects) line
+        all_lines = target_term_subjects.splitlines()
+        if all_lines and "All Subjects" in all_lines[0]:
+            all_lines = all_lines[1:]
+
+        # extract subject code from each line
+        for line in all_lines:
+            subject_match = re.search(r'new Option\(".*?","(.*?)"', line)
+
+            if subject_match:
+                subject_code = subject_match.group(1)
+                self.subjects.append(subject_code)  # add to global subjects list
 
     def parse_courses(self):
-        """
+        """ """
 
-        """
 
 if __name__ == "__main__":
     fetcher = TimetableFetcher("202509", "%")
